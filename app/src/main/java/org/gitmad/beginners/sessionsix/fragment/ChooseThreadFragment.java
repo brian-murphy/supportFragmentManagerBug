c4420a30ba1fb94a9394b3b6bab212e4a3eacb75
Brian Murphy <Brian Murphy>
2016-08-23 09:49:14 -0400

    added typed array of thread images, but recyclerView is slow to load them.

--- app/src/main/java/org/gitmad/beginners/sessionsix/fragment/ChooseThreadFragment.java
+++ app/src/main/java/org/gitmad/beginners/sessionsix/fragment/ChooseThreadFragment.java
@@ -1,6 +1,9 @@
 package org.gitmad.beginners.sessionsix.fragment;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,14 +11,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 
 import org.gitmad.beginners.sessionsix.R;
 
 public class ChooseThreadFragment extends Fragment {
 
-    private TextViewAdapter adapter;
+    private ThreadListAdapter adapter;
 
     private OnThreadClickedListener threadClickListener;
 
@@ -39,7 +40,24 @@ public class ChooseThreadFragment extends Fragment {
 
         String [] threadNames = getResources().getStringArray(R.array.thread_names);
 
-        adapter = new TextViewAdapter(threadNames, threadClickListener);
+        Drawable[] threadImages = getThreadImages();
+
+        adapter = new ThreadListAdapter(threadNames, threadImages, threadClickListener);
+    }
+
+    @NonNull
+    private Drawable[] getThreadImages() {
+        TypedArray threadImageIds = getResources().obtainTypedArray(R.array.thread_image_ids);
+
+        Drawable[] threadImages = new Drawable[threadImageIds.length()];
+
+        for (int i = 0; i < threadImageIds.length(); i++) {
+            threadImages[i] = threadImageIds.getDrawable(i);
+        }
+
+        threadImageIds.recycle();
+
+        return threadImages;
     }
 
     @Override
---
+++ app/src/main/java/org/gitmad/beginners/sessionsix/fragment/ThreadListAdapter.java
@@ -0,0 +1,73 @@
+package org.gitmad.beginners.sessionsix.fragment;
+
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.gitmad.beginners.sessionsix.R;
+
+public class ThreadListAdapter extends RecyclerView.Adapter<ThreadListAdapter.ThreadViewHolder> {
+
+    public static final int NULL_RESOURCE = 0;
+    private String[] listItemStrings;
+    private Drawable[] threadImages;
+    private OnThreadClickedListener itemClickListener;
+
+    public ThreadListAdapter(String[] listItemStrings, Drawable[] threadImages, OnThreadClickedListener itemClickListener) {
+        this.listItemStrings = listItemStrings;
+        this.threadImages = threadImages;
+        this.itemClickListener = itemClickListener;
+}
+
+    @Override
+    public ThreadViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View listItemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.thread_list_item, parent, false);
+
+        listItemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return new ThreadViewHolder(listItemView, itemClickListener);
+    }
+
+    @Override
+    public void onBindViewHolder(final ThreadViewHolder holder, int position) {
+        holder.textView.setText(listItemStrings[position]);
+        holder.imageView.setImageDrawable(threadImages[position]);
+        holder.threadIndex = position;
+    }
+
+    @Override
+    public int getItemCount() {
+        return listItemStrings.length;
+    }
+
+    public static class ThreadViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+        ImageView imageView;
+        int threadIndex;
+
+        public ThreadViewHolder(View listItemView, final OnThreadClickedListener itemClickListener) {
+            super(listItemView);
+
+            listItemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    itemClickListener.onThreadClicked(threadIndex);
+                }
+            });
+
+            textView = (TextView) listItemView.findViewById(R.id.threadNameTextView);
+            imageView = (ImageView) listItemView.findViewById(R.id.threadImageView);
+        }
+    }
+}
